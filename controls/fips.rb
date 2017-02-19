af362bad0b02ea6ae78235c0705ddcba8aaf23ea
Andrey Aleksandrov <postgred@gmail.com>
2017-02-19 21:08:15 +0300

    control for boot params

--- controls/fips.rb
+++ controls/fips.rb
@@ -29,6 +29,7 @@ control 'fips-03' do
   end
 end
 
+# TODO: create issue on github. can't check modules
 control 'fips-04' do
   impact 1.0
   title 'Check crypt modules'
@@ -52,5 +53,19 @@ control 'fips-04' do
 
   describe kernel_module('dm_mod') do
     it { should be_enabled }
+  end
+end
+
+control 'fips-05' do
+  impact 1.0
+  title 'Check grub settings'
+  desc 'Kernel should have fips param and boot part'
+
+  describe grub_conf('/boot/grub2/grub.cfg') do
+    its(:kernel) { should include 'fips=1' }
+  end
+
+  describe grub_conf('/boot/grub2/grub.cfg') do
+    its(:kernel) { should include /boot=/ }
   end
 end
